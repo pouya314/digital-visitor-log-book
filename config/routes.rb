1b3531e2f14065514e4787e61b6c309faa6f44b9
Pouya Arvandian <pouya314@gmail.com>
2014-03-11 14:24:54 +0800

    welcome page added and made the landing page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 FsRapidProto::Application.routes.draw do
+  get "index/welcome"
   resources :visitors
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'visitors#index'
+  root 'index#welcome'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
