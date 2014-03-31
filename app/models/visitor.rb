5e85396869a242647145175057c7b6cd83485f0e
Pouya Arvandian <pouya314@gmail.com>
2014-03-31 16:34:14 +0800

    when we delete a visitor all of the visit records associated with him/her should also be destroyed. but this probably will not happen anyway. no one is going to delete visitor records.

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -1,4 +1,4 @@
 class Visitor < ActiveRecord::Base
   # validates :name, :photo, :department, :email, :phone, :reason, presence: true
-  has_many :visits
+  has_many :visits, :dependent => :destroy
 end
