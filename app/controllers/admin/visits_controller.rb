fe5bbe33ccc14642554d270de336a7b6deb9e3e9
Pouya Arvandian <pouya314@gmail.com>
2014-04-04 11:16:47 +0800

    ransack adjusted.

--- app/controllers/admin/visits_controller.rb
+++ app/controllers/admin/visits_controller.rb
@@ -4,25 +4,19 @@ class Admin::VisitsController < ApplicationController
   before_action :set_visit, only: [:show]
   before_action :authenticate_admin!
   
-  # GET /visits
-  # GET /visits.json
   def index
-    # @search = Visitor.search(params[:q])
-    # @size_returned = @search.result.size
-    # @visitors = @search.result.order(created_at: :desc).page(params[:page]).per(5)
-    
-    @visits = Visit.all
+    @search = Visit.search(params[:q])
+    @size_returned = @search.result(distinct: true).size
+    @visits = @search.result(distinct: true).order(created_at: :desc).page(params[:page]).per(5)
   end
 
 
-  # GET /visits/1
-  # GET /visits/1.json
   def show
   end
 
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+  
     def set_visit
       @visit = Visit.find(params[:id])
     end
