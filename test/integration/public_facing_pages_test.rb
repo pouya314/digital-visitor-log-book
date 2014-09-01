237bc603f869b20c0e7791227559c71aac22326c
Pouya Arvandian <pouya314@gmail.com>
2014-09-02 02:16:43 +0800

    adds some real tests to ensure capybara integration was successful.

--- test/integration/public_facing_pages_test.rb
+++ test/integration/public_facing_pages_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class PublicFacingPagesTest < ActionDispatch::IntegrationTest
-  test "the truth" do
-    assert true
+  test "home page should have the right content" do
+    visit root_url
+    
+    assert_equal true, has_content?('Welcome to F-Secure')
+    within("h2") do
+      assert has_content? 'Digital Visitor Registry Book'
+    end
   end
 end
