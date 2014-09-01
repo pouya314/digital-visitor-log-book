0ca92ed33417645e412a37f11af6fb7043e52339
Pouya Arvandian <pouya314@gmail.com>
2014-09-01 18:56:32 +0800

    Controller tests commented out, because we don't believe in them :)

--- test/controllers/index_controller_test.rb
+++ test/controllers/index_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class IndexControllerTest < ActionController::TestCase
-  test "should get welcome" do
-    get :welcome
-    assert_response :success
-  end
-
+  # test "should get welcome" do
+  #   get :welcome
+  #   assert_response :success
+  # end
 end
