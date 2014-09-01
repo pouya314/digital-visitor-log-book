56dc31c02d1c5c79b3047f4c3e0537a9597b654b
Pouya Arvandian <pouya314@gmail.com>
2014-09-01 18:54:47 +0800

    New inclusion validation for visit model.

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -2,4 +2,5 @@ class Visit < ActiveRecord::Base
   belongs_to :visitor
   
   validates :from_company, :person_to_meet, :company_to_visit, presence: true
+  validates :company_to_visit, inclusion: { in: ["F-Secure", "Home Guru", "Besedo"] }
 end
