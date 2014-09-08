811e64a42a42590b29ee2c8d3f9ac81152382eaa
Pouya Arvandian <pouya314@gmail.com>
2014-09-08 17:35:58 +0800

    adds one integration test..

--- test/integration/public_facing_pages_test.rb
+++ test/integration/public_facing_pages_test.rb
@@ -1,12 +1,20 @@
 require 'test_helper'
 
 class PublicFacingPagesTest < ActionDispatch::IntegrationTest
-  test "home page should have the right content" do
+  test "home page should have the right header(s)" do
     visit root_url
     
     assert_equal true, has_content?('Welcome to F-Secure')
     within("h2") do
       assert has_content? 'Digital Visitor Registry Book'
     end
+  end
+  
+  test "font awesome cdn is on the page" do
+    visit root_url
+    
+    # assert has_xpath?('//link[contains(@href, "//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css")]', :count => 1)
+    page.has_selector?(:xpath, '//script[contains(@src, "bootstrap.js")]')
+    page.has_selector?(:xpath, '//link[contains(@href, "font-awesome.css")]')
   end
 end
