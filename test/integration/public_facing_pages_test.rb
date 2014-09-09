3c6d853394aa15c84d8bc6c5a96eb1de200eabf6
Pouya Arvandian <pouya314@gmail.com>
2014-09-09 14:21:29 +0800

    adds more integration test: for public facing pages.

--- test/integration/public_facing_pages_test.rb
+++ test/integration/public_facing_pages_test.rb
@@ -1,16 +1,17 @@
 require 'test_helper'
 
 class PublicFacingPagesTest < ActionDispatch::IntegrationTest
-  test "home page should have the right header(s)" do
+  test "home page should have the right header(s) and buttons" do
     visit root_url
     
     assert_equal true, has_content?('Welcome to F-Secure')
     within("h2") do
       assert has_content? 'Digital Visitor Registry Book'
     end
+    page.has_selector?(:xpath, '//a[contains(@class, "register-btn")]', :count => 2)
   end
   
-  test "font awesome cdn is on the page" do
+  test "font awesome cdn & bootstrap js are on the page" do
     visit root_url
     
     # assert has_xpath?('//link[contains(@href, "//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css")]', :count => 1)
