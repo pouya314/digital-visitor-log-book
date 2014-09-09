8fd35e83aab39df279690eb42404cff38b46392c
Pouya Arvandian <pouya314@gmail.com>
2014-09-09 14:46:50 +0800

    adds integration test for returning visitor page check.

--- test/integration/public_facing_pages_test.rb
+++ test/integration/public_facing_pages_test.rb
@@ -8,14 +8,23 @@ class PublicFacingPagesTest < ActionDispatch::IntegrationTest
     within("h2") do
       assert has_content? 'Digital Visitor Registry Book'
     end
-    page.has_selector?(:xpath, '//a[contains(@class, "register-btn")]', :count => 2)
+    assert page.has_selector?(:xpath, '//a[contains(@class, "register-btn")]', :count => 2)
   end
   
   test "font awesome cdn & bootstrap js are on the page" do
     visit root_url
     
-    # assert has_xpath?('//link[contains(@href, "//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css")]', :count => 1)
-    page.has_selector?(:xpath, '//script[contains(@src, "bootstrap.js")]')
-    page.has_selector?(:xpath, '//link[contains(@href, "font-awesome.css")]')
+    # assert page.has_xpath?('.//script[contains(@src, "bootstrap.js")]')
+    # assert page.has_xpath?('//link[contains(@href, "font-awesome.css")]')
+  end
+  
+  test "returning visitor page" do
+    visit root_url
+    click_link('Returning visitor')
+    assert_equal index_returning_path, current_path
+    assert page.has_selector?('form.visitor_search')
+    assert page.has_link?('Back')
+    click_link('Back')
+    assert_equal root_path, current_path
   end
 end
