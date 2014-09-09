e8e74f2edefc8444ace9d4d1f98cce33e10cb1d4
Pouya Arvandian <pouya314@gmail.com>
2014-09-09 17:17:56 +0800

    adds integration tests for first time visitor behaviour

--- test/integration/first_time_visitor_test.rb
+++ test/integration/first_time_visitor_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class FirstTimeVisitorTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def go_to_first_time_registration_page
+    visit root_url
+    click_link('First time visitor')
+  end
+  
+  test "should not be able to register without providing required data" do
+    go_to_first_time_registration_page
+    click_button('Submit')
+    
+    assert page.has_selector?('div#error_explanation')
+    assert page.has_content?('All fields are mandatory')
+  end
+  
+  test "visitor should be able to register with all data provided" do
+    go_to_first_time_registration_page
+    
+    # provide some sample image data
+    assert page.has_selector?('a#startbutton')
+    page.click_link('startbutton')
+    page.click_link('ok')
+    
+    fill_in('visitor_name', :with => "Martin")  
+    fill_in('visitor_visits_attributes_0_from_company', :with => "ztory")
+    fill_in('visitor_visits_attributes_0_person_to_meet', :with => "Pouya")
+    select('F-Secure', :from => 'visitor_visits_attributes_0_company_to_visit')
+    click_button('Submit')
+    
+    assert_equal root_path, current_path
+    assert page.has_content?('You may now proceed')
+  end
 end
